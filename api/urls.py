91f9113c28936ce063f3ce4b480e7554b895cdc7
Amaan Poonawala <amaanpoonawala05@gmail.com>
2024-12-09 16:57:13 +0530

    trigger

--- api/urls.py
+++ api/urls.py
@@ -1,28 +1,8 @@
-# # api/urls.py
-
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import ProjectViewSet, BlogPostViewSet
-
-# router = DefaultRouter()
-# router.register(r'projects', ProjectViewSet)
-# router.register(r'blogposts', BlogPostViewSet)
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
-
-# from django.urls import path
-# from .views import HelloWorldView
-
-# urlpatterns = [
-#     path('hello/', HelloWorldView.as_view(), name='hello_world'),
-# ]
-
 from django.urls import path
-from .views import send_email
+from .views import send_email, health_check
 
 urlpatterns = [
     path('send-email/', send_email, name='send_email'),
+    path('health-check/', health_check, name='health_check'),
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,25 +1,3 @@
-# # # api/views.py
-
-# # from rest_framework import viewsets
-# # from .models import Project, BlogPost
-# # from .serializers import ProjectSerializer, BlogPostSerializer
-
-# # class ProjectViewSet(viewsets.ModelViewSet):
-# #     queryset = Project.objects.all()
-# #     serializer_class = ProjectSerializer
-
-# # class BlogPostViewSet(viewsets.ModelViewSet):
-# #     queryset = BlogPost.objects.all()
-# #     serializer_class = BlogPostSerializer
-
-# # from rest_framework.views import APIView
-# # from rest_framework.response import Response
-# # from rest_framework import status
-
-# # class HelloWorldView(APIView):
-# #     def get(self, request):
-# #         return Response({"message": "Hello, World!"}, status=status.HTTP_200_OK)
-
 from django.core.mail import send_mail
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -34,14 +12,19 @@ def send_email(request):
 
         if not name or not user_email or not message:
             return Response({"error": "All fields are required."}, status=400)
+
         email_subject = f"New Contact Form Submission from {name}"
         email_message = f"Name: {name}\nEmail: {user_email}\n\nMessage:\n{message}"
+
         send_mail(
             subject=email_subject,
             message=email_message,
-            from_email=user_email,  
+            from_email=user_email,
             recipient_list=[settings.EMAIL_HOST_USER],
             fail_silently=False,
         )
         return Response({"success": "Email sent successfully!"})
 
+@api_view(['GET'])
+def health_check(request):
+    return Response({"status": "Server is awake!"}, status=200)
