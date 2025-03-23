c9518a37c785def77c104df833628941135545e6
Amaan Poonawala <amaanpoonawala05@gmail.com>
2025-03-23 13:55:19 +0530

    modified urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import send_email, health_check
+from .views import send_email, health_check, send_email_lab
 
 urlpatterns = [
     path('send-email/', send_email, name='send_email'),
+    path('send-email-lab/', send_email_lab, name='send_email_lab'),
     path('health-check/', health_check, name='health_check'),
 ]
 
